56dad4f28014568abd810442436e6f1dd21ce6ff
Gustavo Martinez <gustavo@codeman.company>
2022-10-24 11:52:59 -0500

    Progress Linux

--- request-curl.php
+++ request-curl.php
@@ -1,13 +1,28 @@
 <?php
+//  ██████╗ ██████╗ ██████╗ ███████╗███╗   ███╗ █████╗ ███╗   ██╗
+// ██╔════╝██╔═══██╗██╔══██╗██╔════╝████╗ ████║██╔══██╗████╗  ██║
+// ██║     ██║   ██║██║  ██║█████╗  ██╔████╔██║███████║██╔██╗ ██║
+// ██║     ██║   ██║██║  ██║██╔══╝  ██║╚██╔╝██║██╔══██║██║╚██╗██║
+// ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
+//  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
+// @version 1.0.0
 
-$url = 'https://hooks.slack.com/services/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';
+$url = 'https://hooks.slack.com/services/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';
 $handler = curl_init( $url );
 
 $payload = json_encode( [
+	'color' => 'warning', // Puede tomar los valores good, warning, danger
 	'channel' => '#unimex',
 	'icon_emoji' => 'lizard',
-	'text'	=>	'This is a text.',
+	'text'	=>	'Práctica 3',
 	'username' => 'kuetspali',
+	'fields' => [
+		[
+			'title' => 'Warning',
+			'value' => 'Valor de texto del campo. Puede contener el marcado estándar de mensajes y debe incluir los habituales caracteres de escape. Puede ser multilínea.',
+			'short' => false
+		]
+	],
 ] );
 
 curl_setopt( $handler, CURLOPT_POSTFIELDS, $payload );
